38e3a0d00f76cc976ffaad48140c733ab477b7c3
ShakthiM91 <coolgang.shakthi@gmail.com>
2014-02-20 21:56:20 +0530

    add constructor to TreeNode

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -2,6 +2,13 @@ public class TreeNode {
 	TreeNode left;
 	TreeNode right;
 	
-	String title, aName, aSurname;
+	String bTitle, aName, aSurname;
 	int ISBN;
+	
+	public TreeNode(String bookTitle, int ISBN, String autherName, String autherSurname){
+		this.bTitle = bookTitle;
+		this.ISBN=ISBN;
+		this.aName=autherName;
+		this.aSurname=autherSurname;
+	}
 }
