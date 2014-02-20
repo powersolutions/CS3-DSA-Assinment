b63eacca7a60ad9114e6a6e69a8c76d8ff00c7cf
Isuru R. Kandambi <isururk@gmail.com>
2014-02-20 23:43:33 +0530

    search
    pls check this

--- src/Operations.java
+++ src/Operations.java
@@ -1,24 +1,6 @@
+
 public class Operations {
-	public void insert(TreeNode node, String bookTitle, int ISBN,
-			String autherName, String autherSurname) {
-		if (bookTitle.compareTo(node.bTitle) > 10) {
-			if (node.left != null) {
-				insert(node.right, bookTitle, ISBN, autherName, autherSurname);
-			} else {
-				node.left = new TreeNode(bookTitle, ISBN, autherName,
-						autherSurname);
-			}
-		} else if (bookTitle.compareTo(node.bTitle) < 0) {
-			if (node.right != null) {
-				insert(node.right, bookTitle, ISBN, autherName, autherSurname);
-			} else {
-				node.right = new TreeNode(bookTitle, ISBN, autherName,
-						autherSurname);
-			}
-		}
-	}
-	
-	public void showAll(){
-		
+	public void insert(TreeNode node, String bookTitle, int ISBN, String autherName, String autherSurname){
+		if(bookTitle.)
 	}
 }
