cdfc79c15bcb532ca63a06d01b0c1cbf37562013
ShakthiM91 <coolgang.shakthi@gmail.com>
2014-02-20 22:10:51 +0530

    insert operation added

--- src/Operations.java
+++ src/Operations.java
@@ -1,6 +1,24 @@
-
 public class Operations {
-	public void insert(TreeNode node, String bookTitle, int ISBN, String autherName, String autherSurname){
-		if(bookTitle.)
+	public void insert(TreeNode node, String bookTitle, int ISBN,
+			String autherName, String autherSurname) {
+		if (bookTitle.compareTo(node.bTitle) > 10) {
+			if (node.left != null) {
+				insert(node.right, bookTitle, ISBN, autherName, autherSurname);
+			} else {
+				node.left = new TreeNode(bookTitle, ISBN, autherName,
+						autherSurname);
+			}
+		} else if (bookTitle.compareTo(node.bTitle) < 0) {
+			if (node.right != null) {
+				insert(node.right, bookTitle, ISBN, autherName, autherSurname);
+			} else {
+				node.right = new TreeNode(bookTitle, ISBN, autherName,
+						autherSurname);
+			}
+		}
+	}
+	
+	public void showAll(){
+		
 	}
 }
