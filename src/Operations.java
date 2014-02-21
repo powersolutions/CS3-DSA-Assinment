22a43eb8a3ca4a22053a9224c04336cb98aa6f00
ShakthiM91 <coolgang.shakthi@gmail.com>
2014-02-21 16:04:59 +0530

    insestruction to use insert operations

--- src/Operations.java
+++ src/Operations.java
@@ -4,6 +4,20 @@ import java.util.Queue;
 public class Operations {
 	public static Queue<TreeNode> data = new LinkedList<TreeNode>();
 	
+	/*insert operation
+* to insert a value to the database.
+* first you have to create a object with the TreeNode class
+*
+* lest's assume that the object name is root
+*
+* then create another object with operations class (op)
+*
+* now to insert a value, all you have to do is pass values using the insert method through
+* operations object(op)
+*
+* give the object name you created with the TreeNode as the first parameter. (root)
+*
+*/
 	public void insert(TreeNode node, String bookTitle, int ISBN,
 			String autherName, String autherSurname) {
 		if (bookTitle.length() < node.bTitle.length()) {
