c718298b7bc4f9b8b8c09f89e5faa78508bdb77c
ShakthiM91 <coolgang.shakthi@gmail.com>
2014-02-21 12:14:44 +0530

    added sample application added comments on how to use insert

--- src/Operations.java
+++ src/Operations.java
@@ -3,7 +3,22 @@ import java.util.Queue;
 
 public class Operations {
 	public static Queue<TreeNode> data = new LinkedList<TreeNode>();
+
 	
+	/*insert operation
+	 * to insert a value to the database. 
+	 * first you have to create a object with the TreeNode class
+	 * 
+	 * lest's assume that the object name is root
+	 * 
+	 * then create another object with operations class (op)
+	 * 
+	 * now to insert a value, all you have to do is pass values using the insert method through
+	 * operations object(op)
+	 * 
+	 * give the object name you created with the TreeNode as the first parameter. (root)
+	 *   
+	 */
 	public void insert(TreeNode node, String bookTitle, int ISBN,
 			String autherName, String autherSurname) {
 		if (bookTitle.length() < node.bTitle.length()) {
@@ -14,16 +29,23 @@ public class Operations {
 				node.left = new TreeNode(bookTitle, ISBN, autherName,
 						autherSurname);
 
-		}else if(bookTitle.length()>node.bTitle.length()){
-			if(node.right != null)
+		} else if (bookTitle.length() > node.bTitle.length()) {
+			if (node.right != null)
 				insert(node.right, bookTitle, ISBN, autherName, autherSurname);
 			else
-				node.right = new TreeNode(bookTitle, ISBN, autherName, autherSurname);
+				node.right = new TreeNode(bookTitle, ISBN, autherName,
+						autherSurname);
+		} else if (bookTitle.length() == node.bTitle.length()) {
+			if (node.right != null)
+				insert(node.right, bookTitle, ISBN, autherName, autherSurname);
+			else
+				node.right = new TreeNode(bookTitle, ISBN, autherName,
+						autherSurname);
 		}
 	}
-	
-	public void getAll(TreeNode node){
-		if(node != null){
+
+	public void getAll(TreeNode node) {
+		if (node != null) {
 			getAll(node.left);
 			data.add(node);
 			getAll(node.right);
